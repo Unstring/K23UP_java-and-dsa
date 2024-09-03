e2e7741f92e9dbbba0ab5dbc75016643c4c656ae
Amit Anand <dropmail.amit@gmail.com>
2024-09-03 09:58:19 +0530

    basic syntaxing

---
+++ CSM229/Sep03/properSyntaxing.java
@@ -0,0 +1,25 @@
+// import java.util.Arrays;
+
+class AnotherClass{
+
+}
+
+public class properSyntaxing {
+    int someInt = 1;
+
+    int methodName(){
+        return 1;
+    }
+
+    void thisWontReturnNothing(){
+        System.out.println("this method will not return anything but to print in console");
+    }
+
+    public int addNumbers(int a, int b){
+        return a + b;
+    }
+
+    public static void main(String[] anyArgs){
+        System.out.println("something");
+    }
+}
