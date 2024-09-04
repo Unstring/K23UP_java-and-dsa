dd75de2d00bfee69d12cf1f1aacaaf16ff6b1cb1
Amit Anand <dropmail.amit@gmail.com>
2024-09-04 10:31:48 +0530

    is palindrome using stack and queue

--- CSM229/Sep04/checkPelindrome.java
+++ CSM229/Sep04/checkPelindrome.java
@@ -1,4 +1,7 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class checkPelindrome {
     public static void main(String[] args) {
@@ -9,14 +12,41 @@ public class checkPelindrome {
 
         scn.close();
 
-        boolean isWordPelindrome = ispalindrome(word);
+        // boolean isWordPelindrome = ispalindrome(word);
+        boolean isWordPelindrome = isPalindromeUsingStackAndQueue(word);
 
         System.out.println("Is the word: " + word + " a palindrome: " + isWordPelindrome);
     }
 
     private static boolean ispalindrome(String word) {
 
-        // write your code here!
-        return false;
+        int length = word.length();
+        for (int i = 0; i < length; i++) {
+            Character c0 = word.charAt(i);
+            Character c1 = word.charAt(length - i - 1);
+            if (c0 != c1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isPalindromeUsingStackAndQueue(String word){
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new ArrayDeque<>();
+
+        for(char c : word.toCharArray()){
+            stack.push(c);
+            queue.add(c);
+        }
+
+        while(!stack.isEmpty()){
+            char c0 = stack.pop();
+            char c1 = queue.poll();
+            if(c0!= c1){
+                return false;
+            }
+        }
+        return true;
     }
 }
