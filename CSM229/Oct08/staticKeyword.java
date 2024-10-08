83378cc6213a1827f13ee8e63e20d2a60cb3d076
Amit Anand <dropmail.amit@gmail.com>
2024-10-08 10:06:16 +0530

    setter and getter methods

--- CSM229/Oct08/staticKeyword.java
+++ CSM229/Oct08/staticKeyword.java
@@ -15,13 +15,43 @@ class publicOrPrivate {
     }
 
     public void menupulate(){
-        this.privateName = "updated private name" + this.privateName;
-        this.publicName = "updated public name" + this.publicName;
+        menupulateName();
         // System.out.println(this.privateName);
         // System.out.println(this.publicName);
     }
+    
+    private void menupulateName(){
+        this.privateName = "updated private name" + this.privateName;
+        this.publicName = "updated public name" + this.publicName;
+    }
 }
 
+class setterAndGetter{
+    private String firstName;
+    private String LastName;
+
+    public setterAndGetter(String fullName){
+        String[] names = fullName.split(" ");
+        this.firstName = names[0];
+        this.LastName = names[1];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.LastName = lastName;
+    }
+}
 
 public class staticKeyword {
 
